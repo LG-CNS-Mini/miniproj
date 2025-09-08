5961a3004aa44e72b5251935210b11835ba559ff
hyunsoojin <jhs7251@naver.com>
2025-09-08 17:53:22 +0900

    fe commit test

---
+++ instagram-clone/src/App.jsx
@@ -0,0 +1,16 @@
+import { useContext, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [isLogin, setIsLogin] = useContext(false);
+
+  return (
+    <>
+      isLogin? <AuthedApp /> : <UnAuthedApp />
+    </>
+  )
+}
+
+export default App
