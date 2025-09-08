404c4c9f28310b8770a02256847820b8f30ec6d6
hyunsoojin <jhs7251@naver.com>
2025-09-08 23:58:57 +0900

    백엔드 초기코드 ì커밋

---
+++ be-instagram-clone/src/test/java/com/lgcns/beinstagramclone/BeInstagramCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.beinstagramclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeInstagramCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
