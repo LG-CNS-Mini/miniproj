7e1ef046c278d9ef3a1ec719922e4c456aa8cb76
seongmincho <seongmincho33@gmail.com>
2025-09-11 17:19:38 +0900

    간단 로그인 기능 추가
    회원가입까지는 됨

--- fe-instagram-clone/src/App.jsx
+++ fe-instagram-clone/src/App.jsx
@@ -1,16 +1,25 @@
-import { useContext, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUpPage from "./component/login/page/SignUpPage";
+import SignInPage from "./component/login/page/SignInPage";
+import styled from "styled-components";
+
+const DivTitleText = styled.p`
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
+`;
 
 function App() {
-  const [isLogin, setIsLogin] = useContext(false);
-
-  return (
-    <>
-      isLogin? <AuthedApp /> : <UnAuthedApp />
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <DivTitleText>미니프로젝트7조 인스타그램</DivTitleText>
+            <Routes>
+                <Route path="/" element={<SignUpPage />} />
+                <Route path="/signin" element={<SignInPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
---
+++ fe-instagram-clone/src/api/axios.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const url = import.meta.env.VITE_APP_JSON_SERVER_URL;
+console.log("[debug] >>> ref env : ", url);
+
+const token = localStorage.getItem("accessToken");
+console.log("[debug] >>> api accessToken : ", token);
+
+const api = axios.create({
+    baseURL: url,
+    // baseURL: token ? url + "/auth" : url,
+    // headers: token ? { Authorization: `Bearer ${token}` } : {},
+    withCredentials: true,
+});
+
+export default api;
+
+/*
+비동기 통신을 할 때 브라우저 보안 정책으로 인한 CORS 문제가 발생한다 
+Cross-Origin-Resource Sharing 
+
+인터셉터(request , response) 개념을 이용해서 
+- 인증된 사용자의 액세스토큰 확인
+- 토큰이 만료된 경우 자동 재발급 요청
+*/
