40e6a808377d2fb96433887e26c0be8811d510a8
하지현 <wlgusgk12@kw.ac.kr>
2025-09-16 01:41:11 +0900

    feat:피드 목록/피드 무한스크롤(slice)추가

---
+++ be-instagram-clone/src/main/java/com/lgcns/beinstagramclone/post/domain/dto/SliceResponseDTO.java
@@ -0,0 +1,13 @@
+package com.lgcns.beinstagramclone.post.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.List;
+
+@Data @AllArgsConstructor
+public class SliceResponseDTO<T> {
+    private List<T> items;
+    private int page;
+    private int size;
+    private boolean hasNext;
+}
