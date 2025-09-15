2dffe24f8d3ec356bf62a17f603d57621ba4e486
조재호 <he6465@naver.com>
2025-09-15 14:35:07 +0900

    follow 기능 구현
    follow 기능 구현

--- be-instagram-clone/src/main/java/com/lgcns/beinstagramclone/user/repository/UserRepository.java
+++ be-instagram-clone/src/main/java/com/lgcns/beinstagramclone/user/repository/UserRepository.java
@@ -1,12 +1,16 @@
 package com.lgcns.beinstagramclone.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lgcns.beinstagramclone.user.domain.entity.UserEntity;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, String>{
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 
     public UserEntity findByEmailAndPasswd(String email, String passwd);
+
+    Optional<UserEntity> findByEmail(String email);
 }
