e325dc7e6c6faea6f5e766930f0ec5426ad10e69
hyunsoojin <jhs7251@naver.com>
2025-09-16 16:36:44 +0900

    메인화면 피드 수정

--- be-instagram-clone/src/main/java/com/lgcns/beinstagramclone/post/ctrl/PostCtrl.java
+++ be-instagram-clone/src/main/java/com/lgcns/beinstagramclone/post/ctrl/PostCtrl.java
@@ -6,17 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.lgcns.beinstagramclone.post.domain.dto.PostRequestDTO;
@@ -32,7 +22,7 @@ public class PostCtrl {
 
     @GetMapping("/posts/my")
     public ResponseEntity<List<PostResponseDTO>> myPosts(
-            @RequestHeader("authorEmail") String email) {
+            @RequestParam("authorEmail") String email) {
         List<PostResponseDTO> list = postService.select(email);
         return ResponseEntity.ok(list);
     }
