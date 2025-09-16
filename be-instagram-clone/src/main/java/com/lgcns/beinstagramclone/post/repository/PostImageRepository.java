58744a2cada88dcabaf2025dcf74c8f360960d97
하지현 <wlgusgk12@kw.ac.kr>
2025-09-16 12:11:26 +0900

    feat: 이미지 일괄조회

--- be-instagram-clone/src/main/java/com/lgcns/beinstagramclone/post/repository/PostImageRepository.java
+++ be-instagram-clone/src/main/java/com/lgcns/beinstagramclone/post/repository/PostImageRepository.java
@@ -1,8 +1,14 @@
 package com.lgcns.beinstagramclone.post.repository;
 
+import com.lgcns.beinstagramclone.post.domain.entity.PostImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.lgcns.beinstagramclone.post.domain.entity.PostImageEntity;
+import java.util.Collection;
+import java.util.List;
 
 public interface PostImageRepository extends JpaRepository<PostImageEntity, Integer> {
+
+    // 글 id 목록에 해당하는 이미지 전부 한 번에
+    List<PostImageEntity> findByPost_PostIDIn(Collection<Integer> postIds);
+
 }
