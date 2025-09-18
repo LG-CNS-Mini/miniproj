ea90a784798d1f5ec693018726d1a0000ccfb437
hyunsoojin <jhs7251@naver.com>
2025-09-18 17:46:43 +0900

    테스트 케이스 수정

--- be-instagram-clone/src/test/java/com/lgcns/beinstagramclone/image/service/ImageServicedTest.java
+++ be-instagram-clone/src/test/java/com/lgcns/beinstagramclone/image/service/ImageServicedTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-public class ImageServicedTest2 {
+public class ImageServicedTest {
 
     @Autowired
     private ImageAIService imageAIService;
